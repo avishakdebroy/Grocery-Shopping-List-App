9317c70dcb42ad7145570bcccca2d232c8240b3b
Avishak Deb Roy <avishakdebroy@hotmail.com>
2022-10-19 17:10:43 -0400

    update

--- src/main/java/com/avishak/groceryshoppinglistapp/service/Impl/UserServiceImpl.java
+++ src/main/java/com/avishak/groceryshoppinglistapp/service/Impl/UserServiceImpl.java
@@ -1,7 +1,68 @@
 package com.avishak.groceryshoppinglistapp.service.Impl;
 
+import com.avishak.groceryshoppinglistapp.dto.UserDto;
+import com.avishak.groceryshoppinglistapp.entity.Role;
+import com.avishak.groceryshoppinglistapp.entity.User;
+import com.avishak.groceryshoppinglistapp.repository.RoleRepository;
+import com.avishak.groceryshoppinglistapp.repository.UserRepository;
+import com.avishak.groceryshoppinglistapp.service.UserService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
+
+    private RoleRepository roleRepository;
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserServiceImpl(RoleRepository roleRepository, UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public void saveUser(UserDto userDto) {
+        User user = new User();
+        user.setName(userDto.getFirstName() + " " + userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
+        Role role = roleRepository.findByName("ROLE_USER");
+        if(role == null){
+            role = checkRoleExist();
+        }
+        user.setRoles(Arrays.asList(role));
+        userRepository.save(user);
+    }
+
+    private Role checkRoleExist() {
+        Role role = new Role();
+        role.setName("ROLE_USER");
+        roleRepository.save(role);
+        return role;
+    }
+
+    public List<UserDto> findAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map((user) -> convertEntityToDto(user))
+                .collect(Collectors.toList());
+    }
+
+    private UserDto convertEntityToDto(User user) {
+        UserDto userDto = new UserDto();
+        String[] name = user.getName().split(" ");
+        userDto.setFirstName(name[0]);
+        userDto.setLastName(name[1]);
+        userDto.setEmail(user.getEmail());
+        return userDto;
+    }
+
 }
