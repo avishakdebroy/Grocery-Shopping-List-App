1d580c2d2e1fe98235cb3d62fa4acdeffa7ee3c0
Avishak Deb Roy <avishakdebroy@hotmail.com>
2022-10-19 20:41:29 -0400

    update

--- src/main/java/com/avishak/groceryshoppinglistapp/controller/WishlistController.java
+++ src/main/java/com/avishak/groceryshoppinglistapp/controller/WishlistController.java
@@ -1,10 +1,14 @@
 package com.avishak.groceryshoppinglistapp.controller;
 
+import com.avishak.groceryshoppinglistapp.entity.Wishlist;
 import com.avishak.groceryshoppinglistapp.service.WishlistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WishlistController {
@@ -16,6 +20,32 @@ public class WishlistController {
     public String viewWishlistPage(Model model) {
         model.addAttribute("listWishlists", wishlistService.findAllWishlists());
         return "wishlists";
+    }
+
+    @GetMapping("/add-wishlist")
+    public String addWishlistForm(Model model) {
+        Wishlist wishlist = new Wishlist();
+        model.addAttribute("wishlist", wishlist);
+        return "add_wishlist";
+    }
+
+    @GetMapping("/update-wishlist/{id}")
+    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+        Wishlist wishlist = wishlistService.findWishlistById(id);
+        model.addAttribute("wishlist", wishlist);
+        return "update_wishlist";
+    }
+
+    @GetMapping("/delete-wishlist/{id}")
+    public String deleteWishlist(@PathVariable(value = "id") long id) {
+        this.wishlistService.deleteWishlistById(id);
+        return "redirect:/wishlists";
+    }
+
+    @PostMapping("/save-wishlist")
+    public String saveWishlist(@ModelAttribute("wishlist") Wishlist wishlist) {
+        wishlistService.saveWishlist(wishlist);
+        return "redirect:/wishlists";
     }
 
 }
--- src/main/java/com/avishak/groceryshoppinglistapp/service/Impl/WishlistServiceImpl.java
+++ src/main/java/com/avishak/groceryshoppinglistapp/service/Impl/WishlistServiceImpl.java
@@ -1,4 +1,46 @@
 package com.avishak.groceryshoppinglistapp.service.Impl;
 
-public class WishlistServiceImpl {
+import com.avishak.groceryshoppinglistapp.entity.Wishlist;
+import com.avishak.groceryshoppinglistapp.repository.WishlistRepository;
+import com.avishak.groceryshoppinglistapp.service.WishlistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class WishlistServiceImpl implements WishlistService {
+    @Autowired
+    private WishlistRepository wishlistRepository;
+
+    public WishlistServiceImpl(WishlistRepository wishlistRepository) {
+        this.wishlistRepository = wishlistRepository;
+    }
+
+    @Override
+    public void saveWishlist(Wishlist wishlist) {
+        this.wishlistRepository.save(wishlist);
+    }
+
+    @Override
+    public Wishlist findWishlistById(Long id) {
+        Optional<Wishlist> optional = wishlistRepository.findById(id);
+        Wishlist wishlist = null;
+        if (optional.isPresent()) {
+            wishlist = optional.get();
+        } else {
+            throw new RuntimeException(" Wishlist not found for id :: " + id);
+        }
+        return wishlist;
+    }
+
+    @Override
+    public void deleteWishlistById(Long id) {
+        this.wishlistRepository.deleteById(id);
+    }
+    @Override
+    public List<Wishlist> findAllWishlists() {
+        return wishlistRepository.findAll();
+    }
 }
